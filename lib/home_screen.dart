abcbdf0646c569288d21c00f303ea3280b7b1692
Sayeed <sayeedssaadman272491@gmail.com>
2023-11-03 22:37:25 +0600

    okey

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,91 +8,28 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final TextEditingController _amountOfGlassTEController = TextEditingController(text: '1');
-  List<WaterTrack> waterConsumeList = [];
-  int totalAmount = 0;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
       appBar: AppBar(
-        title: const Text('Water tracker'),
+        title: Center(child: Text('Todo Works')),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Total consume',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                Text(
-                  '$totalAmount',
-                  style: Theme.of(context).textTheme.headlineLarge,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 50,
-                      child: TextField(
-                        controller: _amountOfGlassTEController,
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.number,
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        int amount = int.tryParse(_amountOfGlassTEController.text.trim()) ?? 1;
-                        totalAmount += amount;
-                        WaterTrack waterTrack = WaterTrack(DateTime.now(), amount);
-                        waterConsumeList.add(waterTrack);
-                        setState(() {});
-                      },
-                      child: const Text('Add'),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+          itemCount : 10,
+          itemBuilder: (context,index){
+        return ListTile(
+          leading: CircleAvatar(
+            child: Text("${index+1}"),
           ),
-          Expanded(
-            child: ListView.builder(
-                itemCount: waterConsumeList.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    elevation: 4,
-                    child: ListTile(
-                      onLongPress: () {
-                        print('on long press');
-                        // TODO : Delete the item from list
-                        // TODO : totalAmount theke remove korte hobe
-                      },
-                      leading: CircleAvatar(
-                        child: Text('${index + 1}'),
-                      ),
-                      title: Text(DateFormat('HH:mm:ss  dd-MM-yyyy')
-                          .format(waterConsumeList[index].time)),
-                      trailing: Text(
-                        '${waterConsumeList[index].noOfGlass}',
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                    ),
-                  );
-                }),
-          )
-        ],
-      ),
+          title: Text("i need to go to dhaka"),
+          subtitle: Text("Date"),
+          trailing: Text("pending"),
+        );
+      }),
     );
   }
-}
-
-class WaterTrack {
-  final DateTime time;
-  final int noOfGlass;
-
-  WaterTrack(this.time, this.noOfGlass);
 }
